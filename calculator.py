17fb029b6120cd87bd394509ee50c5449fb30ebe
sreelayaindraganti <sreelayaindraganti@gmail.com>
2026-07-06 15:21:20 +0530

    Added Task 2- Calculator

---
+++ calculator.py
@@ -0,0 +1,26 @@
+print("1 - Add")
+print("2 - Subtract")
+print("3 - Multiplication")
+print("4 - Division")
+option=int(input("Choosen an operation:"))
+
+
+if( option in [1,2,3,4]):
+    num1=int(input("Enter your first number:"))
+    num2=int(input("Enter your second number:"))
+
+    if(option==1):
+       result = num1+num2 
+    elif(option==2):
+        result = num1-num2
+    elif(option==3):
+        result = num1*num2
+    elif(option==4):
+        result = num1/num2
+else:
+    print("invalid option!")
+
+print("The result of the operation is {}".format(result))
+
+
+    
